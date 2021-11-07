376f0da3173d78096ae939d6972f75151bb8cfb2
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2021-11-08 03:42:44 +0800

    add button social button

--- app/src/main/java/com/example/todolistaapps/Profile.java
+++ app/src/main/java/com/example/todolistaapps/Profile.java
@@ -1,6 +1,8 @@
 package com.example.todolistaapps;
 
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -19,6 +21,31 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Profile.this, Dasshboard.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ImageButton btn2 = (ImageButton) findViewById(R.id.imageButton8);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://github.com/KEVINGILBERTTODING"));
+                startActivity(intent);
+            }
+        });
+
+        ImageButton btn3 = (ImageButton) findViewById(R.id.imageButton9);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://github.com/KEVINGILBERTTODING"));
                 startActivity(intent);
             }
         });
